19bfbbd775a23cf501bc8563af8b00e18f589493
mayursakpal7777 <157672861+mayursakpal7777@users.noreply.github.com>
2024-10-16 14:57:19 +0530

    inital commit

---
+++ src/main/java/com/example/demo/EnotesBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesBookApplication.class, args);
+	}
+
+}
