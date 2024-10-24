834a5d262abd5e88fb470f78336ecae108e25b82
mayursakpal7777 <157672861+mayursakpal7777@users.noreply.github.com>
2024-10-24 11:34:20 +0530

    auditing-implements

--- src/main/java/com/example/demo/EnotesBookApplication.java
+++ src/main/java/com/example/demo/EnotesBookApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAware")
 public class EnotesBookApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/entity/BaseModel.java
+++ src/main/java/com/example/demo/entity/BaseModel.java
@@ -2,6 +2,12 @@ package com.example.demo.entity;
 
 import java.util.Date;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,12 +15,23 @@ import lombok.Setter;
 @Getter
 @Setter
 @MappedSuperclass
-public class BaseModel {
-	private Boolean isActive;
-	private Boolean isDeleted;
+public abstract class BaseModel {
+	//private Boolean isActive;
+	//private Boolean isDeleted;
+	@CreatedBy
+	@Column(updatable = false)
 	private Integer createdBy;
+	
+	@CreatedDate
+	@Column(updatable = false)
 	private Date createdOn;
+	
+	@LastModifiedBy
+	@Column(updatable = false)
 	private Integer updatedBy;
+	
+	@LastModifiedDate
+	@Column(updatable = false)
 	private Date updatedOn;
 
 }
