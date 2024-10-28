be36b431af5120fd5923fab20cf062eaae4442ab
Mayur <mayursakpal7745@gmail.com>
2024-10-28 15:38:50 +0530

    generic-rersponse

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -12,7 +12,11 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+<<<<<<< Updated upstream
 import com.example.demo.util.ValidationException;
+=======
+import com.example.demo.util.CommonUtil;
+>>>>>>> Stashed changes
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,21 +28,31 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?>handleException(Exception e)
 	{
 		log.error("GlobalException ::handleException ::",e.getMessage());
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		//return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		return CommonUtil.createErrorResponseMessage(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		
 	}
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<?>handleNullPointerException(Exception e)
 	{
 		log.error("GlobalException ::handleNullPointerFoundException ::",e.getMessage());
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
-		
+		//return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		return CommonUtil.createErrorResponseMessage(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?>handleResourceNotFoundException(Exception e)
 	{
 		log.error("GlobalException ::handleResourceNotFoundException ::",e.getMessage());
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+		//return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+		return CommonUtil.createErrorResponseMessage(e.getMessage(),HttpStatus.NOT_FOUND);
+		
+	}
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<?>handleValidationException(ValidationException e)
+	{
+		//log.error("GlobalException ::handleResourceNotFoundException ::",e.getMessage());
+		//return new ResponseEntity<>(e.getErrors(),HttpStatus.BAD_REQUEST);
+		return CommonUtil.createErrorResponse(e.getErrors(),HttpStatus.BAD_REQUEST);
 		
 	}
 /*	@ExceptionHandler(MethodArgumentNotValidException.class)
@@ -51,11 +65,12 @@ public class GlobalExceptionHandler {
 		allErrors.stream().forEach(er-> {
 			String msg =er.getDefaultMessage();
 			String field = ((FieldError) (er)).getField();
-			error.put(field,  msg);
+			error.put(field,  msg)
 		});
 		return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
 		
 	}*/
+<<<<<<< Updated upstream
 	@ExceptionHandler(ValidationException.class)
 	public ResponseEntity<?>handleValidationException(ValidationException e)
 	{
@@ -63,5 +78,22 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(e.getErrors(),HttpStatus.BAD_REQUEST);
 		
 	}
+=======
+	
+	/*
+	@ExceptionHandler(ExitsDataException.class)
+	public ResponseEntity<?>handleExistDataException(ExitsDataException e)
+	{
+		//log.error("GlobalException ::handleResourceNotFoundException ::",e.getMessage());
+		return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+		
+	}
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?>handleHttpMessageNotReadableException(HttpMessageNotReadableException e)
+	{
+		//log.error("GlobalException ::handleResourceNotFoundException ::",e.getMessage());
+		return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+		*/
+	}
+>>>>>>> Stashed changes
 
-}
